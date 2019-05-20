95df64ece0cadf26af01f1ef382de75823032971
Cath Aylward <aylw0005@algonquinlive.com>
2019-05-20 14:18:07 -0400

    Updates

---
+++ precache-manifest.609c5d050e7bd427995bf0b698c315f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a11cd16db6d490f7f7f53c24769981",
+    "url": "/NPFS-Exercise-v2/index.html"
+  },
+  {
+    "revision": "4ce567bc2b32a59b7b75",
+    "url": "/NPFS-Exercise-v2/static/css/main.bc016b67.chunk.css"
+  },
+  {
+    "revision": "5fca2c286303de9d5954",
+    "url": "/NPFS-Exercise-v2/static/js/2.e2bc4550.chunk.js"
+  },
+  {
+    "revision": "4ce567bc2b32a59b7b75",
+    "url": "/NPFS-Exercise-v2/static/js/main.6e0c34e9.chunk.js"
+  },
+  {
+    "revision": "b1463d6d0a5c85844045",
+    "url": "/NPFS-Exercise-v2/static/js/runtime~main.1d10c5ea.js"
+  }
+]);
